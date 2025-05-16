81afa172f1ab5dca41c93882a52b9116a1f76df0
Pattaradanai Srichon <aprils96950@gmail.com>
2025-05-16 21:30:52 +0700

    Add initial project files and setup for RSpec testing
    - Added icon files: PNG and SVG formats.
    - Created robots.txt for web crawler directives.
    - Added .keep files to maintain directory structure.
    - Implemented RSpec tests for PageHelper, TasksHelper, and Task model.
    - Created request specs for pages and tasks.
    - Defined routing specs for tasks.
    - Set up Rails helper and spec helper files.
    - Added view specs for tasks and pages.
    - Configured application system test case with Selenium.

---
+++ spec/views/tasks/index.html.tailwindcss_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe "tasks/index", type: :view do
+  before(:each) do
+    assign(:tasks, [
+      Task.create!(
+        description: "MyText",
+        completed: false
+      ),
+      Task.create!(
+        description: "MyText",
+        completed: false
+      )
+    ])
+  end
+
+  it "renders a list of tasks" do
+    render
+    cell_selector = 'div>p'
+    assert_select cell_selector, text: Regexp.new("MyText".to_s), count: 2
+    assert_select cell_selector, text: Regexp.new(false.to_s), count: 2
+  end
+end
